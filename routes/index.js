c1bb85ae5704419ebb1c3e08b32e649b9d9151a7
Blake <bthomas@onlocationinc.com>
2016-04-28 19:18:11 -0400

    use a static html

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,6 @@ const await = require('await');
 const fs = require('fs');
 const api = require('./api');
 
-router.get('/', function(req, res){
-	console.error('route: /, ip: %s, time: %s', req.ip, new Date().toTimeString().substr(0,9));
-
-  res.render('index');
-}); 
-
 router.use('/api', api);
 
 module.exports = router;
